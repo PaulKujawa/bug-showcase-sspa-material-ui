fc2ecb5d394ced0fbf27dc8ea575e0873ec52933
Paul Kujawa <herr.kujawa@gmail.com>
2021-09-16 18:23:23 +0200

    swapped situation

--- app/src/root.component.tsx
+++ app/src/root.component.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Button } from "@material-ui/core";
-import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
 import Chip from "@material-ui/core/Chip";
 
 const customTheme = createMuiTheme({
